4ba35bd5a5bb6ef939eab3657053ea8b497d839d
akashchandra-code <akashchandra6280@gmail.com>
2026-01-12 02:06:16 +0530

    notification service done

--- payment/server.js
+++ payment/server.js
@@ -2,6 +2,11 @@ require('dotenv').config();
 const app = require('./src/app');
 const PORT = process.env.PORT || 4000;
 const connectDB = require('./src/db/db');
+const {connect} = require('./src/broker/broker');
+
+
+// Connect to RabbitMQ and set up listeners 
+connect()
 
 // Connect to the database
 connectDB();
